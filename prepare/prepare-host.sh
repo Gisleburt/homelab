6b709f9d71e3b97d393b0d1460a72ee610f101af
Daniel Mason <daniel@danielmason.com>
2021-06-30 00:35:24 +0100

    feat(prepare): improve script automation

--- prepare/prepare-host.sh
+++ prepare/prepare-host.sh
@@ -1,25 +1,37 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -eufo pipefail
 
 SSH_USER=pi
 
-IP=$1
+HOST_TYPE=$1
+HOST_TYPE_REGEX='^(master)|(node)$'
+if ! [[ $HOST_TYPE =~ $HOST_TYPE_REGEX ]] ; then
+  echo "error: first parameter should be either 'master' or 'node'" >&2; exit 1
+fi
+
+IP=$2
 IP_REGEX='^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$'
 if ! [[ $IP =~ $IP_REGEX ]] ; then
-  echo "error: first parameter should be an IPv4 address" >&2; exit 1
+  echo "error: second parameter should be an IPv4 address" >&2; exit 1
 fi
+NODE_ID=$(echo "$IP" | sed 's/[0-9]*\.//g')
 
-NEW_HOSTNAME=$2
-HOSTNAME_REGEX='^[0-9a-zA-Z][0-9a-zA-Z\-]{0,62}$'
-if ! [[ $NEW_HOSTNAME =~ $HOSTNAME_REGEX ]] ; then
-  echo "error: second parameter should be a valid hostname" >&2; exit 1
-fi
+NEW_HOSTNAME="k8s-${HOST_TYPE}-${NODE_ID}"
 
 SSH_KEY=$3
-if ! [[ -f "$SSH_KEY" ]] ; then
-  echo "error: third parameter should be the path to your ssh public key" >&2; exit 1
+if ! [[ -f "${SSH_KEY}" ]] ; then
+  echo "error: third parameter should be the path to your ssh key" >&2; exit 1
 fi
-if [[ $(head -c 3 $SSH_KEY) != "ssh" ]] ; then
+SSH_KEY_REGEX="^\-+BEGIN.+PRIVATE KEY\-$"
+if [[ $(head 1 "${SSH_KEY}") =~ $SSH_KEY_REGEX ]] ; then
+  echo "error: the path at the third parameter was not to an ssh key" >&2; exit 1
+fi
+
+SSH_PUB="${SSH_KEY}.pub"
+if ! [[ -f "${SSH_PUB}" ]] ; then
+  echo "error: could not find " >&2; exit 1
+fi
+if [[ $(head -c 3 "${SSH_PUB}") != "ssh" ]] ; then
   echo "error: the path at the third parameter was not to a public key" >&2; exit 1
 fi
 
@@ -31,14 +43,19 @@ fi
 echo "╔═════════════════╗"
 echo "║ Copying Keyfile ║"
 echo "╚═════════════════╝"
-ssh-copy-id -i $SSH_KEY $SSH_USER@$IP
+ssh-copy-id -i $SSH_PUB $SSH_USER@$IP
+
+echo "╔═════════════════╗"
+echo "║ Change Password ║"
+echo "╚═════════════════╝"
+ssh -i $SSH_KEY -t $SSH_USER@$IP "passwd"
 
 echo "╔══════════════════╗"
 echo "║ Setting Hostname ║"
 echo "╚══════════════════╝"
-ssh -t $SSH_USER@$IP "sudo hostnamectl set-hostname $NEW_HOSTNAME"
+ssh -i $SSH_KEY -t $SSH_USER@$IP "sudo hostnamectl set-hostname $NEW_HOSTNAME"
 
 echo "╔════════════╗"
 echo "║ Restarting ║"
 echo "╚════════════╝"
-ssh -t $SSH_USER@$IP "sudo shutdown -r 0"
+ssh -i $SSH_KEY -t $SSH_USER@$IP "sudo shutdown -r 0"
