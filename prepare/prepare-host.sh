77cb0fcdf726583e7e2e76d79fc49456a129fd1d
Daniel Mason <daniel@danielmason.com>
2026-04-06 22:20:45 +0100

    fix: fix the prepare script

--- prepare/prepare-host.sh
+++ prepare/prepare-host.sh
@@ -1,38 +1,40 @@
 #!/usr/bin/env bash
 set -eufo pipefail
 
-SSH_USER=pi
+# Create the .ssh dir if its not there
+mkdir -p "${PWD}/.ssh"
 
-HOST_TYPE=$1
-HOST_TYPE_REGEX='^(server)|(agent)$'
-if ! [[ $HOST_TYPE =~ $HOST_TYPE_REGEX ]] ; then
-  echo "error: first parameter should be either 'server' or 'agent'" >&2; exit 1
-fi
-
-IP=$2
-IP_REGEX='^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$'
-if ! [[ $IP =~ $IP_REGEX ]] ; then
-  echo "error: second parameter should be an IPv4 address" >&2; exit 1
-fi
-NODE_ID=$(echo "$IP" | sed 's/[0-9]*\.//g')
-
-NEW_HOSTNAME="k8s-${HOST_TYPE}-${NODE_ID}"
-
-SSH_KEY=$3
+# Create the ssh key if its not there
+SSH_KEY="${PWD}/.ssh/homelab"
 if ! [[ -f "${SSH_KEY}" ]] ; then
-  echo "error: third parameter should be the path to your ssh key" >&2; exit 1
+  echo "╔═════════════════════════════╗"
+  echo "║ Keyfile not found, creating ║"
+  echo "╚═════════════════════════════╝"
+  ssh-keygen -f "${SSH_KEY}" -N "" -C "Homelab Auto Generated Key"
+  chmod go-rwx "${SSH_KEY}"
 fi
+
+SSH_USER=$1
+if [[ $SSH_USER =~ "" ]] ; then
+  echo "error: first parameter should be the username for ssh" >&2; exit 1
+fi
+
+HOST=$2
+if [[ $HOST =~ "" ]] ; then
+  echo "error: second parameter should be the hostname for ssh" >&2; exit 1
+fi
+
 SSH_KEY_REGEX="^\-+BEGIN.+PRIVATE KEY\-$"
 if [[ $(head 1 "${SSH_KEY}") =~ $SSH_KEY_REGEX ]] ; then
-  echo "error: the path at the third parameter was not to an ssh key" >&2; exit 1
+  echo "error: ssh key does not seem to have been created correctly" >&2; exit 1
 fi
 
 SSH_PUB="${SSH_KEY}.pub"
 if ! [[ -f "${SSH_PUB}" ]] ; then
-  echo "error: could not find " >&2; exit 1
+  echo "error: could not find ${SSH_PUB}" >&2; exit 1
 fi
 if [[ $(head -c 3 "${SSH_PUB}") != "ssh" ]] ; then
-  echo "error: the path at the third parameter was not to a public key" >&2; exit 1
+  echo "error: ${SSH_PUB} does not appear to be a public key" >&2; exit 1
 fi
 
 #┌─┬┐  ╔═╦╗  ╓─╥╖  ╒═╤╕
@@ -40,22 +42,27 @@ fi
 #├─┼┤  ╠═╬╣  ╟─╫╢  ╞═╪╡
 #└─┴┘  ╚═╩╝  ╙─╨╜  ╘═╧╛
 
-#echo "╔═════════════════╗"
-#echo "║ Copying Keyfile ║"
-#echo "╚═════════════════╝"
-#ssh-copy-id -i $SSH_PUB $SSH_USER@$IP
+echo "╔═════════════════╗"
+echo "║ Copying Keyfile ║"
+echo "╚═════════════════╝"
+ssh-copy-id -i $SSH_PUB $SSH_USER@$HOST
+
+echo "╔════════════════════╗"
+echo "║ Testing Connection ║"
+echo "╚════════════════════╝"
+ssh -i $SSH_KEY -t $SSH_USER@$HOST "echo \"Hello, World!\""
 #
 #echo "╔═════════════════╗"
 #echo "║ Change Password ║"
 #echo "╚═════════════════╝"
-#ssh -i $SSH_KEY -t $SSH_USER@$IP "passwd"
+#ssh -i $SSH_KEY -t $SSH_USER@$HOST "passwd"
 
-echo "╔══════════════════╗"
-echo "║ Setting Hostname ║"
-echo "╚══════════════════╝"
-ssh -i $SSH_KEY -t $SSH_USER@$IP "sudo hostnamectl set-hostname $NEW_HOSTNAME"
+#echo "╔══════════════════╗"
+#echo "║ Setting Hostname ║"
+#echo "╚══════════════════╝"
+#ssh -i $SSH_KEY -t $SSH_USER@$HOST "sudo hostnamectl set-hostname $NEW_HOSTNAME"
 
-echo "╔════════════╗"
-echo "║ Restarting ║"
-echo "╚════════════╝"
-ssh -i $SSH_KEY -t $SSH_USER@$IP "sudo shutdown -r 0"
+#echo "╔════════════╗"
+#echo "║ Restarting ║"
+#echo "╚════════════╝"
+#ssh -i $SSH_KEY -t $SSH_USER@$HOST "sudo shutdown -r 0"
